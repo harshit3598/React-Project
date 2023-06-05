04203c32eb7c43b031ac17610222187dbe2201ac
harshit3598 <harshitsanghvi46@gmail.com>
2023-06-04 17:00:55 -0700

    Selection from a list version working

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,5 +1,27 @@
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
-function App(){
-  return <div><Message></Message></div>
+function App() {
+  let items = [
+    "New York",
+    "San Francisco",
+    "Los Angeles",
+    "San Diego",
+    "Boston",
+  ];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
+
+export default App;
--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,5 +1,9 @@
 function Message(){
     //JSX: JavaScript XML
+    const name = 'Harshit';
+    if (name)
+        return <h1>Hello {name}</h1>
+    return <h1>Hello world</h1>
     return <h1>Hello World</h1>;
 }
 
