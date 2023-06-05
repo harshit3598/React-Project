226cd67ee4cc1813e78f22811646fa43070a5707
harshit3598 <harshitsanghvi46@gmail.com>
2023-06-04 17:13:25 -0700

    added parent and child support version in Alert

--- react-app/src/components/Alert.tsx
+++ react-app/src/components/Alert.tsx
@@ -1,7 +1,12 @@
-const Alert = () => {
-  return (
-    <div>Alert</div>
-  )
+import { ReactNode } from "react";
+
+interface AlertProps {
+  //to pass HTML content and children keyword for parent component and child component
+  children: ReactNode;
 }
 
-export default Alert
+const Alert = ({ children }: AlertProps) => {
+  return <div className="alert alert-primary">{children}</div>;
+};
+
+export default Alert;
